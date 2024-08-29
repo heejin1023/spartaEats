84c0973d1ed5cc3218afc4eb19013c503f8bb32e
jdm2131 <jdm2131@naver.com>
2024-08-29 18:32:27 +0900

    Custom Exception 추가

---
+++ src/main/java/com/sparta/spartaeats/exception/DeletedProductException.java
@@ -0,0 +1,19 @@
+package com.sparta.spartaeats.exception;
+
+public class DeletedProductException extends RuntimeException {
+    public DeletedProductException() {
+        super();
+    }
+
+    public DeletedProductException(String message) {
+        super(message);
+    }
+
+    public DeletedProductException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DeletedProductException(Throwable cause) {
+        super(cause);
+    }
+}
